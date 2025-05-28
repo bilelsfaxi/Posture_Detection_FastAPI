d5dde77e24d303cdf4b1538194125530d3829b7e
Votre Nom <sfaxi.bilel@enis.tn>
2025-05-29 00:32:49 +0100

    upload

--- service/main.py
+++ service/main.py
@@ -1,17 +1,9 @@
 from fastapi import FastAPI
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 from service.routers import routers_yolo11
 
 app = FastAPI(title="YOLOv11 Dog Posture Detection API")
 app.include_router(routers_yolo11.router)
 
-@app.on_event("startup")
-async def startup_event():
-    logger.info("Application startup complete. Server is running.")
 
 @app.get("/")
 async def root():
