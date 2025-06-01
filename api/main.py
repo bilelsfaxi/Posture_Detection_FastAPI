81077dc5354a60d7364afaf33383a638d1d6e04c
Votre Nom <sfaxi.bilel@enis.tn>
2025-06-01 16:12:52 +0100

    new update

--- api/main.py
+++ api/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 import logging
-from api.detectors import detectors_yolo11 # Importez le module
+from api.detectors import detectors_yolo11
+from api.routers import routers_yolo11
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-from api.routers import routers_yolo11
 
 app = FastAPI(title="YOLOv11 Dog Posture Detection API")
 app.include_router(routers_yolo11.router)
@@ -17,6 +16,6 @@ detector = detectors_yolo11.YOLOv11Detector(model_url="https://drive.google.com/
 async def startup_event():
     logger.info("Application startup complete. Server is running.")
 
-@app.get("/")
+@app.get("/")      
 async def root():
     return {"message": "YOLOv11 Dog Posture Detection API. Use POST /yolo/predict to upload an image."}
